93447be74eac1fdc03bf2b1994fd25d13f738758
francisco <franciscojaviersernavaldez123@gmail.com>
2020-11-30 13:41:19 -0700

    P1 commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = [
+    { 
+        path: '', 
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+    
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NopagefoundComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash:true })],
+    exports: [RouterModule]
+})
+export class APP_ROUTES {}

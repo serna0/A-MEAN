07aa20cdfa43a4bd1ee769e3c31fddc866bf2079
francisco <franciscojaviersernavaldez123@gmail.com>
2020-12-03 00:17:02 -0700

    Input, output y viewvChield

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 //Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
 //RUTAS
 import { APP_ROUTES } from './app.routes';
 //Componentes
@@ -10,17 +11,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     PagesModule,
     APP_ROUTES,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 //Rutas 
 import { PAGES_ROUTES } from './pages.routes'; 
 //Modulos
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 @NgModule({
     declarations: [
@@ -17,11 +23,15 @@ import { PagesComponent } from './pages.component';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     imports: [ 
         CommonModule,
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES, 
+        FormsModule,
+        ChartsModule
      ],
     exports: [
         DashboardComponent,
